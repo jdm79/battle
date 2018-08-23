1b76a524877e7f96969b06a7850560fa2831ef62
Alastair Edmonds <alastair.edmonds@gmail.com>
2018-08-23 16:31:15 +0100

    updated names to use global vars

--- app.rb
+++ app.rb
@@ -1,24 +1,31 @@
 require 'sinatra/base'
-require 'player'
+require './lib/player'
 
 class Battle < Sinatra::Base
     enable :sessions
+
+    $p1 = 'John'
+
 
   get '/' do
    erb(:index)
   end
 
   post '/names' do
-    session[:player_one] = params[:player_one] #name= in form
-    session[:player_two] = params[:player_two]
+    # session[:player_one] = params[:player_one] #name= in form
+    # session[:player_two] = params[:player_two]
+    $p1 = Player.new(params[:player_one])
+    $p2 = Player.new(params[:player_two])
     session[:player_one_points] = 100
     session[:player_two_points] = 100
     redirect '/play'
   end
 
   get '/play' do 
-    @p1 = session[:player_one]
-    @p2 = session[:player_two]
+    # @p1 = session[:player_one]
+    # @p2 = session[:player_two]
+    @p1 = $p1.name
+    @p2 = $p2.name
     @player_one_points = session[:player_one_points]
     @player_two_points = session[:player_two_points]
     erb(:play)
@@ -26,14 +33,13 @@ class Battle < Sinatra::Base
 
   get '/attack' do
     # @player_two_points.to_i - 10
-    @p1 = session[:player_one]
-    @p2 = session[:player_two]
+    # @p1 = session[:player_one]
+    # @p2 = session[:player_two]
+    @p1 = $p1.name
+    @p2 = $p2.name
     erb(:attack)
   end
 
-  # get '/points' do
-  #   erb(:points)
-  # end
 
   run! if app_file == $0
 
