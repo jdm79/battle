2d1d632bd6cd9806e7849cfb6a35c6ba47bca222
Alastair Edmonds <alastair.edmonds@gmail.com>
2018-08-23 16:19:57 +0100

    before refatoring with global variables

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -2,9 +2,16 @@ require 'player'
 
 
 describe Player do
+subject(:player) { Player.new('test') }
 
-
-
-
+  describe '#initialize' do 
+    it 'should know its name' do
+      p1 = Player.new('John')
+      expect(p1.name).to eq('John')
+      end
+    it 'should know its name' do 
+      expect(subject.name).to eq('test')  
+    end
+  end
 
 end
